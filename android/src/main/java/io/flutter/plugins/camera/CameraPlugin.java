2252e901c0e20d908a52e460e5a63a14e73a2486
kienld <75771331+kienkaka150499@users.noreply.github.com>
2025-07-30 11:51:46 +0700

    Update Gradle configuration and add new library dependencies
    - Updated Gradle version to 8.10 in gradle-wrapper.properties.
    - Modified config.properties with the new java.home timestamp.
    - Added new properties files and lock files for Gradle execution history and dependencies.
    - Created new module files for camera_android and its test configurations.
    - Introduced multiple new library definitions for dependencies in the .idea/libraries directory.
    - Commented out deprecated code in CameraPlugin.java to prevent crashes

--- android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
+++ android/src/main/java/io/flutter/plugins/camera/CameraPlugin.java
@@ -43,16 +43,16 @@ public final class CameraPlugin implements FlutterPlugin, ActivityAware {
    * <p>Calling this automatically initializes the plugin. However plugins initialized this way
    * won't react to changes in activity or context, unlike {@link CameraPlugin}.
    */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(
-      @NonNull io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    CameraPlugin plugin = new CameraPlugin();
-    plugin.maybeStartListening(
-        registrar.activity(),
-        registrar.messenger(),
-        registrar::addRequestPermissionsResultListener,
-        registrar.view());
-  }
+//  @SuppressWarnings("deprecation")
+//  public static void registerWith(
+//      @NonNull io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
+//    CameraPlugin plugin = new CameraPlugin();
+//    plugin.maybeStartListening(
+//        registrar.activity(),
+//        registrar.messenger(),
+//        registrar::addRequestPermissionsResultListener,
+//        registrar.view());
+//  }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
